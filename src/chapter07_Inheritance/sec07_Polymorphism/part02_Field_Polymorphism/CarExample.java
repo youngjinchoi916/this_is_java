77ca67cdd6c008c751afce15449c5e023b1a73f3
youngjinchoi916 <defectio916@gmail.com>
2021-01-12 11:02:43 +0900

    Inheritance - Polymorphism(2.Field Polymorphism)

--- src/chapter07_Inheritance/sec07_Polymorphism/part02_Field_Polymorphism/CarExample.java
+++ src/chapter07_Inheritance/sec07_Polymorphism/part02_Field_Polymorphism/CarExample.java
@@ -5,26 +5,28 @@ public class CarExample {
 		Car car = new Car();
 		
 		for(int i=1; i<=5; i++) {
-			int problemLocation = car.run();
+			int problemLocation = car.run();		//타이어 전체를 한바퀴 굴리고, 펑크난 위치를 리턴받음.ㄴ
 			switch(problemLocation) {
-				case 1:
+				case 1:	//앞왼쪽
 					System.out.println("앞왼쪽 HankookTire로 교체");
 					car.frontLeftTire = new HankookTire("앞왼쪽", 15);	//자동 타입 변환 
 					break;
-				case 2:
+				case 2:	//앞오른쪽
 					System.out.println("앞오른쪽 KumhoTire로 교체");
 					car.frontRightTire = new KumhoTire("앞오른쪽", 13);	//자동 타입 변환
 					break;
-				case 3:
+				case 3:	//뒤왼쪽
 					System.out.println("뒤왼쪽 HankookTire로 교체");
 					car.backLeftTire = new HankookTire("뒤왼쪽", 14);		//자동 타입 변환
 					break;
-				case 4:
+				case 4:	//뒤오른쪽
 					System.out.println("뒤오른쪽 KumhoTire로 교체");
 					car.backRightTire = new KumhoTire("뒤오른쪽", 17);	//자동 타입 변환
 					break;
 			}
 			System.out.println("----------------------------------------");
-		}
-	}
-}
+		} //end for
+		
+	} //end main
+	
+} //end class
