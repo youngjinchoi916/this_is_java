cc4553321584d8fd22a10fe54fb69d2a8e17d4e9
youngjinchoi916 <defectio916@gmail.com>
2021-01-12 11:40:56 +0900

    Inheritance - Polymorphism(4.Method Polymorphism)

--- src/chapter07_Inheritance/sec07_Polymorphism/part04_Method_Polymorphism/DriverExample.java
+++ src/chapter07_Inheritance/sec07_Polymorphism/part04_Method_Polymorphism/DriverExample.java
@@ -4,16 +4,19 @@ public class DriverExample {
 	public static void main(String[] args) {
 		Driver driver = new Driver();
 		
-		Bus bus = new Bus();
+		Vehicle vehicle = new Vehicle();
+		
+		driver.drive(vehicle);
 		
 		//운전기사가 버스를 운전한다.
-		//Vehicle vehicle = bus;  -> 자동 타입 변환
+		//Vehicle vehicle = new Bus;  -> 자동 타입 변환
 		//Bus 객체의 오버라이딩된 drive() 메소드 호출
-		driver.drive(bus);
+		driver.drive(new Bus());
 		
 		//운전기사가 택시를 운전한다.
 		//Vehicle vehicle = new Taxi();  -> 자동 타입 변환
 		//Taxi 객체의 오버라이딩된 drive() 메소드 호출
 		driver.drive(new Taxi());
 	}
-}
+
+} //end class
--- src/chapter07_Inheritance/sec07_Polymorphism/part04_Method_Polymorphism/Taxi.java
+++ src/chapter07_Inheritance/sec07_Polymorphism/part04_Method_Polymorphism/Taxi.java
@@ -1,8 +1,11 @@
 package chapter07_Inheritance.sec07_Polymorphism.part04_Method_Polymorphism;
 
 public class Taxi extends Vehicle {
+	
+	//Vehicle의 run() 메소드 재정의
 	@Override
 	public void run() {
 		System.out.println("택시가 달립니다.");
 	}
-}
+	
+} //end class
