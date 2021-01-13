5b22cf434f8f49fd0a3b4b1d3eed21f080d54a72
youngjinchoi916 <defectio916@gmail.com>
2021-01-13 12:08:36 +0900

    Interface - Use Interface(6.디폴트메소드 사용)

--- src/chapter08_Interface/sec04_Use_Interface/part02_Default_Method/Audio.java
+++ src/chapter08_Interface/sec04_Use_Interface/part02_Default_Method/Audio.java
@@ -5,19 +5,22 @@ public class Audio implements RemoteControl {
 	private int volume;
 	private boolean mute;
 	
-	//turnOn() 추상 메소드의 실체 메소드
+	//RemoteControl의 추상메소드 재정의
+	@Override
 	public void turnOn() {
 		System.out.println("Audio를 켭니다.");
-	}	
-	//turnOff() 추상 메소드의 실체 메소드
+	}
+
+	@Override
 	public void turnOff() {
 		System.out.println("Audio를 끕니다.");
 	}
-	//setVolume() 추상 메소드의 실체 메소드
+
+	@Override
 	public void setVolume(int volume) {
-		if(volume>RemoteControl.MAX_VOLUME) {
+		if (volume > RemoteControl.MAX_VOLUME) {
 			this.volume = RemoteControl.MAX_VOLUME;
-		} else if(volume<RemoteControl.MIN_VOLUME) {
+		} else if (volume < RemoteControl.MIN_VOLUME) {
 			this.volume = RemoteControl.MIN_VOLUME;
 		} else {
 			this.volume = volume;
@@ -25,16 +28,16 @@ public class Audio implements RemoteControl {
 		System.out.println("현재 Audio 볼륨: " + volume);
 	}
 	
-	//인터페이스의 디폴트 메소드 재정의
+	/* 인터페이스의 디폴트 메소드 재정의 */
 	//재정의한 디폴트메소드에는 default 키워드를 붙이지 않는다.
 	@Override
 	public void setMute(boolean mute) {
 		this.mute = mute;
 		if(mute) {
-			System.out.println("Audio 무음 처리합니다.");
+			System.out.println("Audio를 무음 처리합니다.");
 		} else {
-			System.out.println("Audio 무음 해제합니다.");
+			System.out.println("Audio를 무음 해제합니다.");
 		}
 	}	
 
-}
+} //end class
--- src/chapter08_Interface/sec04_Use_Interface/part02_Default_Method/RemoteControlExample.java
+++ src/chapter08_Interface/sec04_Use_Interface/part02_Default_Method/RemoteControlExample.java
@@ -1,10 +1,13 @@
 package chapter08_Interface.sec04_Use_Interface.part02_Default_Method;
 
 public class RemoteControlExample {
+	
 	public static void main(String[] args) {
-		/* 인터페이스 사용 */
-		//인터페이스로 구현 객체를 사용하려면 다음과 같이 인터페이스 변수를 선언하고 구현객체를 대입해야 한다.
-		//인터페이스 변수는 참조 타입이기 때문에 구현객체가 대입될 경우 구현 객체의 번지를 저장한다.
+		/*
+		 * [인터페이스 사용]
+		 * 	 - 인터페이스로 구현 객체를 사용하려면 다음과 같이 인터페이스 변수를 선언하고 구현객체를 대입해야 한다.
+		 *  - 인터페이스 변수는 참조 타입이기 때문에 구현객체가 대입될 경우 구현 객체의 번지를 저장한다.
+		 */
 		RemoteControl rc = null;
 		
 		/* Television 객체를 인터페이스 타입에 대입 */
@@ -15,12 +18,15 @@ public class RemoteControlExample {
 		rc.turnOn();
 		/*
 		 * [디폴트 메소드의 사용]
-		 * 		- 디폴트 메소드는 추상메소드가 아닌 인스턴스 메소드이므로 구현객체가 있어야 사용할 수 있다.
-		 * 		  예를들어, RemoteControl 인터페이스는 setMute() 메소드를 가지고 있지만, 다음과 같이 사용할 수 없다.
-		 * 				-> RemoteControl.setMute(true);
+		 * 	 - 디폴트 메소드는 추상메소드가 아닌 인스턴스 메소드이므로 구현객체가 있어야 사용할 수 있다.
+		 * 	    예를들어, RemoteControl 인터페이스는 setMute() 메소드를 가지고 있지만, 다음과 같이 사용할 수 없다.
+		 * 			-> RemoteControl.setMute(true); (x)
+		 * 			-> rc.setMute(true); 	(o)
 		 */
 		//인터페이스의 setMute 메소드(디폴트 메소드) 호출 -> RemoteControl 인터페이스에 선언된 디폴트메소드 호출(구현객체에서 재정의 하지 않았음)
 		rc.setMute(true);
+		
+		System.out.println();
 		
 		/* Audio 객체를 인터페이스 타입에 대입 */
 		//rc에는 RemoteControl 인터페이스를 구현한 Audio 객체의 번지가 저장됨
@@ -30,5 +36,7 @@ public class RemoteControlExample {
 		rc.turnOn();
 		//인터페이스의 디폴트 메소드를 재정의한 Audio 객체의 setMute() 메소드 실행
 		rc.setMute(true);
-	}
-}
+		
+	} //end main
+	
+} //end class
