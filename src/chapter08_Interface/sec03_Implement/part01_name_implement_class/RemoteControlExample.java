24546eea2972f75f5d2622811b79ea8c706b4fdf
youngjinchoi916 <defectio916@gmail.com>
2021-01-13 11:16:15 +0900

    Interface - Implements(1.구현클래스)

--- src/chapter08_Interface/sec03_Implement/part01_name_implement_class/RemoteControlExample.java
+++ src/chapter08_Interface/sec03_Implement/part01_name_implement_class/RemoteControlExample.java
@@ -1,12 +1,13 @@
 package chapter08_Interface.sec03_Implement.part01_name_implement_class;
 
 public class RemoteControlExample {
+	
 	public static void main(String[] args) {
 		
 		//Television tv = new Television();
 		//Television타입 변수에 대입한다고 인터페이스를 사용하는 것이 아니다
 		
-		//인터페이스로 구현 객체를 사용하려면 다음과 같이 인터페이스 변수를 선언하고 구현 객체를 대입해야 한다.
+		//인터페이스로 구현 객체를 사용하려면 다음과 같이 인터페이스 변수를 선언하고 구현 객체(구현 클래스를 객체로 생성해서)를 대입해야 한다.
 		//인터페이스 변수는 참조 타입이기 때문에 구현객체가 대입될 경우 구현 객체의 번지를 저장한다.
 		RemoteControl rc;
 		
@@ -19,9 +20,14 @@ public class RemoteControlExample {
 		rc = new Television();
 		//rc = new Audio();
 
+		//(1) 개발코드가 RemoteControl 인터페이스의 추상메소드 호출 
+		//(2) RemoteControl 인터페이스는 RemoteControl을 구현한 Television 객체의 실체 메소드 실행
+		//(3) Television 객체의 실체 메소드 실행 결과 리턴값 있으면 RemoteControl 로 리턴
+		//(4) RemoteControl에서 리턴값을 받아 개발코드로 리턴 -> 결국 실체메소드의 리턴값이 개발코드로 넘어 온다.
 		rc.turnOn();
 		rc.setVolume(10);
 		rc.turnOff();
 		
-	}
-}
+	} //end main
+	
+} //end class
--- src/chapter08_Interface/sec03_Implement/part01_name_implement_class/Television.java
+++ src/chapter08_Interface/sec03_Implement/part01_name_implement_class/Television.java
@@ -1,13 +1,19 @@
 package chapter08_Interface.sec03_Implement.part01_name_implement_class;
 
+//구현클래스
+//구현클래스가 생성자에 의해 생성되면 구현 객체라 한다.
 public class Television implements RemoteControl {
+	/*
+	 * [인터페이스 구현]
+	 * 	 - 구현 객체 : 인터페이스에서 정의된 추상메소드와 동일한 메소드 이름, 매개 타입, 리턴 타입을 가진 실체 메소드를 갖는 객체
+	 * 	 - 구현 클래스 : 구현 객체를 생성하는 클래스(implements 키워드)
+	 */
 	//필드
 	private int volume;
 	
 	/*
 	 * [추상메소드들에 대한 실체 메소드를 작성할 때 주의할 점]
-	 *  - 인터페이스의 모든 메소드는 기본적으로 public 접근제한을 갖기 때문에
-	 *     public 보다 더 낮은 접근 제한으로 작성할 수 없다.
+	 *  - 인터페이스의 모든 메소드는 기본적으로 public 접근제한을 갖기 때문에 public 보다 더 낮은 접근 제한으로 작성할 수 없다.
 	 */
 	//turnOn() 추상메소드의 실체 메소드
 	@Override
@@ -33,4 +39,5 @@ public class Television implements RemoteControl {
 		}
 		System.out.println("현재 TV 볼륨 : " + volume);
 	}
-}
+
+} //end class
