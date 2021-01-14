459f5afdb07e80008d56700a97ede3b41c4563a7
youngjinchoi916 <defectio916@gmail.com>
2021-01-14 14:34:02 +0900

    Interface - Polymorphism(5.강제타입변환)

--- src/chapter08_Interface/sec05_Polymorphism/part05_Casting/Bus.java
+++ src/chapter08_Interface/sec05_Polymorphism/part05_Casting/Bus.java
@@ -1,12 +1,16 @@
 package chapter08_Interface.sec05_Polymorphism.part05_Casting;
 
 public class Bus implements Vehicle {
+	
+	//인터페이스의 추상메소드 재정의
 	@Override
 	public void run() {
 		System.out.println("버스가 달립니다.");
 	}
 	
+	//구현 객체만 가지고 있는 메소드
 	public void checkFare() {
 		System.out.println("승차요금을 체크 합니다.");
 	}
-}
+
+} //end class
--- src/chapter08_Interface/sec05_Polymorphism/part05_Casting/Vehicle.java
+++ src/chapter08_Interface/sec05_Polymorphism/part05_Casting/Vehicle.java
@@ -1,5 +1,7 @@
 package chapter08_Interface.sec05_Polymorphism.part05_Casting;
 
 public interface Vehicle {
+	
 	public void run();
-}
+
+} //end interface
