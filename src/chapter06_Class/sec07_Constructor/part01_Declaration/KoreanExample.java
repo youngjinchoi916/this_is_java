e062e85ef8d6b9a06c908b75668015fc5e711eb3
youngjinchoi916 <defectio916@gmail.com>
2021-02-10 12:20:31 +0900

    Class - 클래스 구성 멤버(2.생성자 - 선언 방법)

--- src/chapter06_Class/sec07_Constructor/part01_Declaration/KoreanExample.java
+++ src/chapter06_Class/sec07_Constructor/part01_Declaration/KoreanExample.java
@@ -4,6 +4,7 @@ public class KoreanExample {
 
 	public static void main(String[] args) {
 		//기본 생성자가 없으므로 생성 불가
+		//명시적 생성자가 선언되어 있고, 기본 생성자 선언이 없다면 기본 생성자로 객체 생성 불가능
 		//Korean k = new Korean();   
 		
 		Korean k1 = new Korean("박자바", "011225-1234567");
@@ -14,6 +15,6 @@ public class KoreanExample {
 		System.out.println("k2.name : " + k2.name);
 		System.out.println("k2.ssn : " + k2.ssn);
 
-	}
+	} //end main
 
-}
+} //end class
