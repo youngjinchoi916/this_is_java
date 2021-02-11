d2f90e73c89c0d95527fd0ebb72de35653e9d6fc
youngjinchoi916 <defectio916@gmail.com>
2021-02-11 11:16:46 +0900

    Class - 정적 멤버(2.정적 초기화 블록)

---
+++ src/chapter06_Class/sec10_StaticMember/part02_StaticBlock/TelevisionExample.java
@@ -0,0 +1,12 @@
+package chapter06_Class.sec10_StaticMember.part02_StaticBlock;
+
+public class TelevisionExample {
+
+	public static void main(String[] args) {
+		
+		//정적 초기화 블록으로 선언된 정적 필드 사용
+		System.out.println(Television.info);
+		
+	} //end main
+
+} //end class
