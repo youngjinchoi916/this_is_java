9f02a54800a727bedabc72a0c2f55e9100e45202
youngjinchoi916 <defectio916@gmail.com>
2021-02-11 11:11:57 +0900

    Class - 정적 멤버(1.선언방법)

--- src/chapter06_Class/sec10_StaticMember/part01_Declaration/CalculatorExample.java
+++ src/chapter06_Class/sec10_StaticMember/part01_Declaration/CalculatorExample.java
@@ -3,10 +3,12 @@ package chapter06_Class.sec10_StaticMember.part01_Declaration;
 public class CalculatorExample {
 
 	public static void main(String[] args) {
-		//정적멤버(필드, 메소드)는 객체 생성없이 클래스명으로 직접 접근 가능하다.
-		//객체 생성 후 객체 참조 변수로도 사용가능 하지만 권장하지 않는다. -> 이클립스에서는 경고 표시가 나타난다.
+		/* 
+		 * 정적멤버(필드, 메소드)는 객체 생성없이 클래스명으로 직접 접근 가능하다.
+		 * 객체 생성 후 객체 참조 변수로도 사용가능 하지만 권장하지 않는다. -> 이클립스에서는 경고 표시가 나타난다.
+		 */
 		//Calculator cal = new Calculator();
-		//double result1 = 10 * 10 * cal.pi;
+		//double result1 = 10 * 10 * cal.pi;		//참조변수로 Calculator 객체의 정적 필드에 접근 할 수 있으나, 권장하지 않음.
 		
 		double result1 = 10 * 10 * Calculator.pi;
 		int result2 = Calculator.plus(10, 5);
