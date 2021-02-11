7d9aa50b6e71b1a14d25b462da5e02828348f381
youngjinchoi916 <defectio916@gmail.com>
2021-02-11 10:47:32 +0900

    Class - 인스턴스 멤버(feat.this)

--- src/chapter06_Class/sec09_InstanceMember/Car.java
+++ src/chapter06_Class/sec09_InstanceMember/Car.java
@@ -3,9 +3,10 @@ package chapter06_Class.sec09_InstanceMember;
 public class Car {
 	/*
 	 * [인스턴스 멤버] - 인스턴스 필드, 인스턴스 메소드
-	 * 	 - 인스턴스 : 클래스로부터 생성된(new 연산자) 객체를 인스턴스라고 한다.
+	 * 	 - 인스턴스 : 클래스로부터 new 연산자를 통해 생성된 객체를 인스턴스라고 한다.
 	 *  - 인스턴스 멤버란 객체(인스턴스)를 생성한 후 사용할 수 있는 필드와 메소드를 말한다. 
 	 *  - 인스턴스 필드와 메소드는 객체에 소속된 멤버이기 때문에 객체 없이는 사용할 수 없다.
+	 *  - 객체 내부에서도 인스턴스 멤버에 접근 하기 위해서 this 키워드를 사용할 수 있다.
 	 */
 	
 	//필드
@@ -25,6 +26,7 @@ public class Car {
 	
 	public void run() {
 		for(int i=10; i<=50; i+=10) {
+			//인스턴스 멤버임을 명시하기 위해 this 키워드를 사용
 			this.setSpeed(i);
 			System.out.println(this.model + "가 달립니다.(시속:" + this.speed + "km/h)");
 		}
--- src/chapter06_Class/sec09_InstanceMember/CarExample.java
+++ src/chapter06_Class/sec09_InstanceMember/CarExample.java
@@ -3,10 +3,10 @@ package chapter06_Class.sec09_InstanceMember;
 public class CarExample {
 
 	public static void main(String[] args) {
-
+		//객체 생성(Car의 인스턴스 멤버 사용 가능)
 		Car myCar = new Car("포르쉐");
 		Car yourCar = new Car("벤츠");
-
+		
 		myCar.run();
 		yourCar.run();
 	
