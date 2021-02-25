7694c077cd202f431d7542f727e12eb1b874e1be
youngjinchoi916 <defectio916@gmail.com>
2021-02-25 17:49:23 +0900

    Generic - 제한된 타입 파라미터(1.basic)

--- src/chapter13_Generic/sec05_BoundedTypeParameter/part01/BoundedExample.java
+++ src/chapter13_Generic/sec05_BoundedTypeParameter/part01/BoundedExample.java
@@ -1,6 +1,7 @@
 package chapter13_Generic.sec05_BoundedTypeParameter.part01;
 
 public class BoundedExample {
+	
 	public static void main(String[] args) {
 		//Number 타입 또는 Number의 하위 타입 외에는 매개값으로 들어 올 수 없다.
 		//String str = Util.compare("a", "b");     //String 타입은 매개값으로 사용 불가능
@@ -10,6 +11,7 @@ public class BoundedExample {
 		
 		int result2 = Util.compare(4.5, 3);
 		System.out.println(result2);
-	}
-}
-
+	
+	} //end main
+	
+} //end class
--- src/chapter13_Generic/sec05_BoundedTypeParameter/part01/Util.java
+++ src/chapter13_Generic/sec05_BoundedTypeParameter/part01/Util.java
@@ -5,7 +5,6 @@ public class Util {
 	 * [제한된 타입 파라미터 <T extends 최상위타입> ] : 제네릭 메소드에서 사용
 	 *  - 타입 파라미터에 지정 되는 구체적인 타입을 제한할 필요가 종종 있다.
 	 *  - 예를 들어, 숫자를 연산하는 제네릭 메소드는 매개값으로 Number 타입 또는 Number의 하위 타입(Byte, Short, Integer, Long, Double)의 인스턴스만 가져야 한다.
-	 *  
 	 *  - 제한된 타입 파라미터를 선언하려면 타입 파라미터 뒤에 extends 키워드를 붙이고 상위 타입을 명시하면 된다.
 	 *  - 상위 타입은 클래스 뿐만 아니라 인터페이스도 가능하다. 인터페이스라고 해서 implements를 사용하지 않는다.
 	 */
@@ -28,4 +27,5 @@ public class Util {
 		//Double.compare() : 두 개의 숫자 타입을 매개값으로 받아 첫번째 매개값이 작으면 -1, 같으면 0, 크면 1을 리턴한다.
 		return Double.compare(v1, v2);
 	}
-}
+	
+} //end class
